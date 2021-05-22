e2464c477f70ff351d46435942828bf59e0c9056
frunox <jrcannon@msn.com>
2021-05-22 12:29:52 -0500

    end of section 5 caching

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -6,9 +6,14 @@ if (!window.Promise) {
 }
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function () {
-    console.log('Service worker registered!');
-  });
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 window.addEventListener('beforeinstallprompt', function (event) {
@@ -17,58 +22,3 @@ window.addEventListener('beforeinstallprompt', function (event) {
   deferredPrompt = event;
   return false;
 });
-
-var promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('This is executed once the timer is done');
-    // reject({ code: 500, message: 'An error happened' });
-  }, 3000);
-});
-
-// fetch GET request
-fetch('https://httpbin.org/ip')
-  .then((response) => {
-    console.log('FETCH', response);
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log('ERROR', err);
-  });
-
-// fetch POST request
-fetch('https://httpbin.org/post', {
-  method: 'POST',
-  headers: {
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-  },
-  body: JSON.stringify({ message: 'Working???' }),
-})
-  .then((response) => {
-    console.log('FETCH', response);
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data.json);
-  })
-  .catch((err) => {
-    console.log('ERROR', err);
-  });
-
-promise
-  .then((text) => {
-    return text;
-  })
-  .then((newText) => {
-    console.log(newText);
-  })
-  .catch((err) => {
-    console.log('Promise error', err.code, err.message);
-  });
-
-console.log('This is executed after setTimeout is called');
--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,69 @@
+let CACHE_STATIC_NAME = 'static-v4';
+let CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
 self.addEventListener('install', function (event) {
   console.log('[SW] installing SW..', event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME).then((cache) => {
+      console.log('SW: Precaching app shell');
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/promise.js',
+        '/src/js/fetch.js',
+        'https://code.getmdl.io/1.3.0/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://code.getmdl.io/1.3.0/material.indigo-pink.min.css',
+      ]);
+      // cache individual files
+      // cache.add('/');
+      // cache.add('/index.html');
+      // cache.add('/src/js/app.js');
+    })
+  );
 });
 
 self.addEventListener('activate', function (event) {
   console.log('[SW] activating SW..', event);
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key != CACHE_STATIC_NAME && key != CACHE_DYNAMIC_NAME) {
+            console.log('SW  remove old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', function (event) {
-  console.log('[SW] fetch event', event.request.url);
-  event.respondWith(fetch(event.request));
+  // console.log('[SW] fetch event', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        // console.log('already cached');
+        return response;
+      } else {
+        // console.log('fetching...');
+        return fetch(event.request)
+          .then((res) => {
+            return caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
+              cache.put(event.request.url, res.clone());
+              return res;
+            });
+          })
+          .catch((err) => {});
+      }
+    })
+  );
 });
